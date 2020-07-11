e68efed0887b81d55392d3a6ebceb449b249a2d6
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-07-11 00:56:02 -0700

    trouble shooting heroku

--- controllers/burgerscontroller.js
+++ controllers/burgerscontroller.js
@@ -12,7 +12,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burgerModel.createOne([
+    burgerModel.insertOne([
         "burger_name"
     ], [
         req.body.burger_name
@@ -31,7 +31,7 @@ router.put("/api/burgers/:id", function(req, res) {
     burgerModel.updateOne(
         {devoured: req.body.devoured}, condition, function(result) {
         if (result.changedRows === 0) {
-            return res.status(404).end();
+            // return res.status(404).end();
         } else {
             res.status(200).end();
         }
